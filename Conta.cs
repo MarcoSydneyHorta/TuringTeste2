deebe07fb1e4cbadb6d243cb3f3ff1bb8f7cb6be
MarcoSydneyHorta <marco.sh@outlook.com>
2022-02-22 11:35:48 -0300

    Add project files.

---
+++ Conta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public class Conta
+    {
+        
+        public static int Resultado(int[] entrada)
+        {
+
+            int index = 0;
+            int tam = entrada.Length;
+            int elem = 0;
+            int soma = 0;
+            int contador = 0;
+
+            for (int i = 0;i < tam; i++)
+            {
+                elem = entrada[i];
+                for (int j = 0; j < tam; j++)
+                {
+                    if (j != i)
+                    {
+                        if ((entrada[j] != 0) & (elem != 0))
+                        {
+                            soma = elem + entrada[j];
+                            index = Array.IndexOf(entrada, soma);
+                            if (index > -1) contador++;
+                        }
+                    }
+                }
+            }
+            entrada[1] = contador;
+            return entrada[1];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite os dados separados por vírgula: ");
+            string opus = Console.ReadLine();
+            string[] sem = opus.Split(",");
+            int tam = sem.Length;
+            int[] dados = new int[tam];
+
+            for ( int i = 0; i < tam; i++)
+            {
+                dados[i] = int.Parse(sem[i]);
+            }
+            //int[] dados = { 1, 3, 2, 3, 5, 0 };
+
+
+            int saida = Conta.Resultado(dados);
+            Console.Write("Soma = " + saida +  " - " + dados[1]);
+            Console.Read();
+
+        }
+    }
+}
